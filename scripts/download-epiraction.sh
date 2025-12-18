dae6142c4f2dd3ae2dd59a8ac0382d94bb0e5bb6
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2025-12-18 09:16:05 +0000

    feat: add epiraction download from source

--- scripts/download-epiraction.sh
+++ scripts/download-epiraction.sh
@@ -4,45 +4,58 @@ set -euo pipefail
 pwd
 
 # Test for required commands
-for cmd in curl bgzip tabix bedtools parallel; do
+for cmd in bgzip tabix bedtools parallel uvx; do
     if ! command -v "$cmd" &> /dev/null; then
         echo "Error: $cmd is not installed." >&2
+        echo "Please try 'brew install $cmd' or equivalent." >&2
         exit 1
     fi
 done
 
 
 echo "Starting"
-DIR=$(dirname "$0")
+DIR=$(dirname $(dirname "$0"))
+
+echo "$DIR"
 if [ ! -d "$DIR/data" ]; then
     echo "data directory not found, try running the script from the project root"
     exit 1
 fi
 
-readonly DATA_DIR="$DIR/data/intermediate_files/"
-readonly MANIFEST="$DIR/manifest.txt"
+
+readonly DATA_DIR="$DIR/data/25.06/EPIraction/raw"
+readonly MANIFEST="$DIR/data/25.06/epiraction-manifest.tsv"
+
 
 if [ ! -f "$MANIFEST" ]; then
     echo "manifest file not found at $MANIFEST"
     exit 1
 fi
 
+if [  -d "$DATA_DIR" ]; then
+    echo "data directory $DATA_DIR already exists, skipping"
+    exit 0
+
+else
+    echo "Creating data directory at $DATA_DIR"
+    mkdir -p "$DATA_DIR"
+fi
+
 
 echo "Changing to data directory"
-cd "$DATA_DIR" || exit 1
 
 echo "Downloading Epiraction dataset from ENCODE"
-xargs -n 1 curl -O -L < "$MANIFEST"
+time uvx --from git+https://github.com/project-defiant/encode-crawler@v0.1.0 crawler --output-dir "$DATA_DIR" --input "$MANIFEST" > /dev/null 2>&1
 echo "Download complete."
 
 echo "Unzipping"
-find . -name 'ENC*.bed.gz' | parallel 'bgzip -d {}'
+time find . -name 'ENC*.bed.gz' | parallel 'bgzip -d {}'
 echo "Sorting"
-find . -name 'ENC*.bed' | parallel 'bedtools sort -i {} > {.}.sorted.bed'
+time find . -name 'ENC*.bed' | parallel "head -1 {} | sed '1s%#%%' > {.}.sorted.bed && bedtools sort -i {} >> {.}.sorted.bed"
 echo "Block gzipping"
-find . -name 'ENC*.sorted.bed' | parallel 'bgzip {}'
+time find . -name 'ENC*.sorted.bed' | parallel 'bgzip {}'
 echo "Indexing"
-find . -name 'ENC*.sorted.bed.gz' | parallel 'tabix -p bed {}'
+time find . -name 'ENC*.sorted.bed.gz' | parallel 'tabix -p bed -S 1 {}'
 echo "Cleaning up intermediate files"
-find . -name 'ENC*.bed' -type f -delete
+time find . -name 'ENC*.bed' -type f -delete
 echo "Completed."
--- src/manuscript_methods/maf.py
+++ src/manuscript_methods/maf.py
@@ -6,8 +6,10 @@ from collections.abc import Callable
 from enum import StrEnum
 
 import numpy as np
-from pyspark.sql import Column
+from pyspark.ml.feature import Bucketizer
+from pyspark.sql import Column, DataFrame
 from pyspark.sql import functions as f
+from pyspark.sql import types as t
 
 
 class MinorAlleleFrequencyType(StrEnum):
@@ -380,3 +382,86 @@ class MinorAlleleFrequencyBin:
     def bins(self) -> Column:
         """Get the buns."""
         return self._expr.alias("mafBins")
+
+
+def prepare_maf_bins(effect_data: DataFrame, splits: list[float]) -> DataFrame:
+    """Prepare MAF bins using Bucketizer and return the transformed DataFrame.
+
+    Args:
+        effect_data (DataFrame): Input DataFrame containing variant effect data with "maf" column.
+        splits (list[float]): List of float values defining the MAF bin edges.
+
+    Returns:
+        DataFrame: Transformed DataFrame with MAF bins.
+
+    Examples:
+    --------
+        >>> data = [(0.0099, "t1", 0, 2.0), (0.01, "t1", 1, 3.0), (0.2, "t1", 0, 1.5), (0.35, "t1", 1, 4.0), (0.5, "t1", 0, 0.5)]
+        >>> schema = "maf DOUBLE, studyType STRING, isProteinAltering SHORT, absEstimatedBeta DOUBLE"
+        >>> splits = [0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
+        >>> df = spark.createDataFrame(data, schema)
+        >>> prepare_maf_bins(df, splits=splits).select("maf", "mafBin.*").drop("mafBin").orderBy("mafBinIndex").show()
+        Using following buckets [0.0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
+        +------+-----------+-----------+-----------+--------------+-----------+
+        |   maf|mafBinRange|mafBinLower|mafBinUpper|mafBinMidpoint|mafBinIndex|
+        +------+-----------+-----------+-----------+--------------+-----------+
+        |0.0099|   0.0-0.01|        0.0|       0.01|         0.005|          0|
+        |  0.01|  0.01-0.05|       0.01|       0.05|          0.03|          1|
+        |   0.2|    0.2-0.3|        0.2|        0.3|          0.25|          4|
+        |  0.35|    0.3-0.4|        0.3|        0.4|          0.35|          5|
+        |   0.5|    0.4-0.5|        0.4|        0.5|          0.45|          6|
+        +------+-----------+-----------+-----------+--------------+-----------+
+        <BLANKLINE>
+
+    """
+    assert all(isinstance(v, float) for v in splits), "All split values must be floats"
+    assert all(0.0 <= v <= 0.5 for v in splits), "All split values must be between 0.0 and 0.5"
+    assert set(["studyType", "maf", "isProteinAltering", "absEstimatedBeta"]).issubset(effect_data.columns), (
+        "Input DataFrame is missing required columns"
+    )
+    splits = sorted(splits)
+
+    bucketizer = Bucketizer()
+    print(f"Using following buckets {splits}")
+    bucketizer.setSplits(splits)
+    # Use "maf" column as input to bucketizer
+    bucketizer.setInputCol("maf")
+    # Output column will be "mafBin"
+    bucketizer.setOutputCol("mafBin")
+    # Throw error if invalid value - below or above the split bounds is found
+    bucketizer.setHandleInvalid("error")
+
+    # Expression to map the bucketizer output (which are zero-based indices) to actual bin ranges
+    maf_bin_expr = f.when(f.lit(False), f.lit(None))
+    for i in range(len(splits) - 1):
+        maf_bin_expr = maf_bin_expr.when(
+            f.col("mafBin") == i,
+            f.struct(
+                f.lit(f"{splits[i]}-{splits[i + 1]}").alias("mafBinRange"),
+                f.lit(splits[i]).alias("mafBinLower"),
+                f.lit(splits[i + 1]).alias("mafBinUpper"),
+                f.lit(np.round((splits[i] + splits[i + 1]) / 2, 3)).alias("mafBinMidpoint"),
+                f.lit(i).alias("mafBinIndex"),
+            ),
+        )
+
+    ds = (
+        bucketizer.transform(
+            effect_data.select(
+                "studyType",
+                "maf",
+                "isProteinAltering",
+                "absEstimatedBeta",
+            )
+        )
+        # Since the output of the bucketizer are the zero-based indices of the bins, we cast the mafBin to IntegerType
+        # And we get the actual bin ranges using maf_expression
+        .withColumn("mafBin", f.col("mafBin").cast(t.IntegerType()))
+        .withColumn("mafBin", maf_bin_expr)
+        .cache()
+    )
+
+    # Sanity check - assert that the number of bins is the same as splits - 1
+    num_bins = ds.select("mafBin.mafBinIndex").distinct().count()
+    assert num_bins <= (len(splits) - 1), f"Expected {len(splits) - 1} bins but found {num_bins}"
+    return ds
